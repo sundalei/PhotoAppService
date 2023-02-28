9d91d2c5018b8826c05eda61adc3620e4566b782
Leo Sun <leosun@Leos-MacBook-Pro.local>
2023-02-28 19:41:24 +0800

    actuator

--- PhotoAppApiGateWay/src/main/java/com/example/PhotoAppApiGateWayApplication.java
+++ PhotoAppApiGateWay/src/main/java/com/example/PhotoAppApiGateWayApplication.java
@@ -1,8 +1,11 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,11 @@ public class PhotoAppApiGateWayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoAppApiGateWayApplication.class, args);
+	}
+
+	@Bean
+	public HttpExchangeRepository httpTraceRepository() {
+		return new InMemoryHttpExchangeRepository();
 	}
 
 }
